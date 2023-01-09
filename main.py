1627793b61de68fd99ef1bc65a183814ac41adcf
Mee\Mahmoud <mahmoud.asri.hamdan@gmail.com>
2023-01-09 23:14:54 +0200

    rest of the website

--- main.py
+++ main.py
@@ -7,8 +7,19 @@ from chat_bot import ChatBot
 
 @app.route('/')
 def home_page():
-    ChatBot.begin()
     return render_template('index.html')
+
+
+@app.route('/chatbot', methods=['GET'])
+def chatbot_page():
+    ChatBot.begin()
+    return render_template('chatbot.html')
+
+
+@app.route('/rating', methods=['GET'])
+def rating_page():
+    return render_template('rating.html')
+    
 
 
 @app.route('/get-response', methods=['GET'])
